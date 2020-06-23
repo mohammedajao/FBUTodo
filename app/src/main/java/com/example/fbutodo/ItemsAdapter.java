363b51a2c4dcc92bdf724bc2d2529fe1cdba3338
Mohammed Ajao <mohammedajao@fb.com>
2020-06-23 19:23:49 -0400

    Added item adapters to render the modeled list view to the app

---
+++ app/src/main/java/com/example/fbutodo/ItemsAdapter.java
@@ -0,0 +1,61 @@
+package com.example.fbutodo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+// Responsible for taking the data at a particular position and put it into the view holder
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        // Use a layout inflator to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        // We'll create a new view and wrap it inside a new viewholder
+        return new ViewHolder(todoView);
+    }
+
+    // Responsible for binding data to a ViewHolder instance
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        // Grab the item at position
+        String item = items.get(position);
+        // Bind it to the viewholder instance
+        viewHolder.bind(item);
+    }
+
+    // Tells recyclerView how many items are inside the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    // Container to add easy access to views that represent each row of the list
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        // Update view inside of view holder with this data
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+}
--- app/src/main/java/com/example/fbutodo/MainActivity.java
+++ app/src/main/java/com/example/fbutodo/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.fbutodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
+
+    Button btnAdd;
+    EditText etitem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etitem = findViewById(R.id.etitem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Go to the gym");
+        items.add("Have fun");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }
