eb6ba3310c4017526b7b96f92527231b665932c1
Mohammed Ajao <mohammedajao@fb.com>
2020-06-23 20:42:24 -0400

    Added persistence with IO File reader & writer to update collection

--- app/src/main/java/com/example/fbutodo/MainActivity.java
+++ app/src/main/java/com/example/fbutodo/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,10 +35,7 @@ public class MainActivity extends AppCompatActivity {
         etItem = findViewById(R.id.etItem);
         rvItems = findViewById(R.id.rvItems);
 
-        items = new ArrayList<>();
-        items.add("Buy Milk");
-        items.add("Go to the gym");
-        items.add("Have fun");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 // Notify the adapter
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(),"You removed an item", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         };
         itemsAdapter = new ItemsAdapter(items, onLongClickListener);
@@ -61,9 +65,34 @@ public class MainActivity extends AppCompatActivity {
 
                 // Embellishment to notify the user about their action
                 Toast.makeText(getApplicationContext(), "Item was added, ", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         });
 
 
+    }
+
+    private File getDataFile() {
+        // Gets the directory of app and creates a data.txt file
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // Load items by reading every line of the data file
+
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error: Reading items from data.txt", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error: Saving items to data.txt", e);
+        }
     }
 }
